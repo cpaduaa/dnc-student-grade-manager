765ff22adffe260433b9f28ad8b76902c78ed1b9
c.paduaa <cris.padua.96@gmail.com>
2025-08-10 18:42:27 +0100

    PUT and Delete insart

--- index.js
+++ index.js
@@ -13,14 +13,34 @@ const server = http.createServer((req, res) => {
 
     request.on('end', () => {
         if (url === '/grades' && method === 'GET'){
-        response.writeHead(200, {'constent-Type': 'application/json'})
-        response.end(JSON.stringify(grades))
+          response.writeHead(200, {'constent-Type': 'application/json'})
+          response.end(JSON.stringify(grades))
         } else if (url === '/grades' && method === 'POST'){
           const {student, subject, grade} = JSON.parse(body);
           const newGrade = { id: v4(), student, subject, grade }
           grades.push(newGrade);
           response.writeHead(201, {'Content-Type': 'application/json'});
           response.end(JSON.stringify(newGrade));
+        } else if (url.startsWith ('/grades/') && method == "PUT") {
+            const { sutdentName, subject, grade } = JSON.parse(body);
+            const gradeToUpdate = grades.find((g) => g.id === id);
+            if (gradeToUpdate) {
+                gradeToUpdate.student = studentName;
+                gradeToUpdate.subject = subject;
+                gradeToUpdate.grade = grade;
+                res.writeHead(200, {"Content-Type":"application/json"});
+                res.end(JSON.stringify({ massage: "Grade not found" }));
+            }
+        } else if (url.startsWith('/grades/') && method === 'DELETE'){
+            const index = grades.findIndex((g) => g.id === id)
+            if (index !== -1) {
+                grades.splice(index, 1);
+                response.writeHead(204);
+                response.end();
+            } else {
+                response.writeHead(404, {'Content-Type': 'application/json'});
+                response.end(JSON.stringify({message: 'Grade not found'}));
+            }
         } else {
         response.writeHead(404, {'Content-Type': 'text/plain'});
         response.end(JSON.stringify({message: 'Route not found'}));
